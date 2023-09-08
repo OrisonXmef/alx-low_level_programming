52005cde7e824dbcaf0862fcaa35c863fdcb85e2
OrisonXmef <orisonmustapha@gmail.com>
2023-09-07 19:07:28 -0700

    101

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <stdlib.h>
+
 /**
  * main - Entry point
  *
@@ -8,8 +9,8 @@
  */
 int main(void)
 {
-char *message ="and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-write(STDERR_FILENO, message, 59);
+	char *message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	write(STDERR_FILENO, message, 59);
 
 	return (1);
 }
