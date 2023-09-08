b43dded3e21399e34b2c68baed887340ab95c3a8
OrisonXmef <orisonmustapha@gmail.com>
2023-09-08 13:24:30 -0700

    0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,8 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+#include <time.h>
 int main(void) {
   int n;
+  srand(time(NULL));
+   int n = rand() - RAND_MAX / 2;
    n = rand();
     while (n)
     {
