12cf2b0c33e4b33429a4b3ac14e88d76c7d5ab7f
OrisonXmef <orisonmustapha@gmail.com>
2023-09-07 19:26:17 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -9,8 +9,8 @@
  */
 int main(void)
 {
-char *message= "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-	write(STDERR_FILENO, message, 59);
+char *mesage = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	write(STDERR_FILENO, mesage, 59);
 
 	return (1);
 }
