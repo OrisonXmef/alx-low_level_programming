65e4119248251b799aab5a07139d6f529dff15ac
OrisonXmef <orisonmustapha@gmail.com>
2023-09-08 15:59:39 -0700

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,12 @@
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+int main(void)
+{
+putchar(48);
+putchar(49);
+putchar('\n');
+return (0);
+}
