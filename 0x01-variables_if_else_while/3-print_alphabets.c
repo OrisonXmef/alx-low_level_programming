6f39f263bc6cf5e1b546e5a4664e00359a2fc562
OrisonXmef <orisonmustapha@gmail.com>
2023-09-08 16:53:04 -0700

    3-print_alphabets.c

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -4,19 +4,19 @@
  *
  * Return: Always 0 (Success)
  */
-int main() {
-    char lowercase = 'a';
-    char uppercase = 'A';
-    while (lowercase <= 'z') {
-        putchar(lowercase);
-        lowercase++;
-    }
-    while (uppercase <= 'Z') {
-        putchar(uppercase);
-        uppercase++;
-    }
-
-    putchar('\n'); 
-
-    return 0;
+int main(void) 
+{
+char lowercase = 'a';
+char uppercase = 'A';
+while (lowercase <= 'z') {
+putchar(lowercase);
+lowercase++;
+}
+while (uppercase <= 'Z') 
+{
+putchar(uppercase);
+uppercase++;
+}
+putchar('\n'); 
+return (0);
 }
