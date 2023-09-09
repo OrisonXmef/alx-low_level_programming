90a4b0d16e8f561648eccb5f576f53fc77c953d9
OrisonXmef <orisonmustapha@gmail.com>
2023-09-08 18:15:47 -0700

    101-print_comb4.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
- /**
+/**
  * main - Entry point
  *
  * Return: Always 0 (Success)
@@ -8,9 +8,9 @@ int main(void)
 {
 int i;
 int j;
-for (i = 0; i <100; i++)
+for (i = 0; i < 100; i++)
 {
-for (j = i + 1; j <100; j++)
+for (j = i + 1; j < 100; j++)
 {
 putchar((i / 10) + '0');
 putchar((i % 10) + '0');
@@ -19,7 +19,7 @@ putchar((j / 10) + '0');
 putchar((j % 10) + '0');
 if (i == 98 && j ==  99)
 {
-continue;}
+continue; }
 putchar(',');
 putchar(' ');
 }
