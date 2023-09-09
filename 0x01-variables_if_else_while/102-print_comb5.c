78f8ff229920006d32af2b423104eafc520763ea
OrisonXmef <orisonmustapha@gmail.com>
2023-09-08 18:02:02 -0700

    101-print_comb4.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -13,10 +13,10 @@ for (i = 0; i <100; i++)
 for (j = i + 1; j <100; j++)
 {
 putchar((i / 10) '0');
-putchar((i % 10) '0';)
+putchar((i % 10) '0');
 putchar(' ');
 putchar((j / 10) '0');
-putchar((j % 10) '0';)
+putchar((j % 10) '0');
 if (i != 8 || j != 9)
 {
 putchar(',');
