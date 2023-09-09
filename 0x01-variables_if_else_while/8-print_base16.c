dd43e2ee25374396c7889b2bbb95e43622426d03
OrisonXmef <orisonmustapha@gmail.com>
2023-09-08 17:07:34 -0700

    8-print_base16.c

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -7,14 +7,14 @@
 int main(void)
 {
 char digit;
-for (digit = '0'; digit <= '9'; digit++) 
+for (digit = '0'; digit <= '9'; digit++)
 {
 putchar(digit);
 }
-for (digit = 'a'; digit <= 'f'; digit++) 
+for (digit = 'a'; digit <= 'f'; digit++)
 {
 putchar(digit);
 }
 putchar('\n');
-return 0;
+return (0);
 }
