2f9a6cd0778194638ddead80d61ae625259d6f04
OrisonXmef <orisonmustapha@gmail.com>
2023-09-08 18:09:48 -0700

    101-print_comb4.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -17,11 +17,11 @@ putchar((i % 10) + '0');
 putchar(' ');
 putchar((j / 10) + '0');
 putchar((j % 10) + '0');
-if (i != 8 || j != 9)
+if (i == 98 && j ==  99)
 {
+continue;}
 putchar(',');
 putchar(' ');
-}
 }
 }
 putchar('\n');
