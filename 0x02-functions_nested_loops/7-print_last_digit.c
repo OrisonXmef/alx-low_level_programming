383aa7d97ceeebf606adc171ca79ca4427603891
OrisonXmef <orisonmustapha@gmail.com>
2023-09-12 17:17:45 -0700

    7-print_last_digit.c

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -8,10 +8,13 @@
  */
 int print_last_digit(int n)
 {
-	int last_digits;
-	if (n<0)
+	int last_digit;
+
+	if (n < 0)
 		n = -n;
-	last_digits = n % 10;
-	_putchar(last_digits + '0');
-	return last_digits;
+
+	last_digit = n % 10;
+	_putchar(last_digit + '0');
+
+	return last_digit;
 }
