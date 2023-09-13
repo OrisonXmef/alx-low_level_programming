0bc27a36ddb1bbaf3016cb276bfcd2044bb44f50
OrisonXmef <orisonmustapha@gmail.com>
2023-09-13 09:38:34 -0700

    11-print_to_98.c

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -7,8 +7,8 @@
  */
 void print_to_98(int n)
 {
-    if (n <= 98)
-    {
+	if (n <= 98)
+	{
         while (n <= 98)
         {
             printf("%d", n);
@@ -17,8 +17,8 @@ void print_to_98(int n)
             n++;
         }
     }
-    else
-    {
+	else
+	{
         while (n >= 98)
         {
             printf("%d", n);
