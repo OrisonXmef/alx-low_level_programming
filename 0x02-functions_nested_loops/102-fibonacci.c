f518c5000aff2b9bb6fbbbe579d3dd6412c771f6
OrisonXmef <orisonmustapha@gmail.com>
2023-09-13 12:41:12 -0700

    102-fibonacci.c

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -5,27 +5,30 @@
  *
  * Return: Always 0 (Success)
  */
-int main(void) 
+int main(void)
 {
-    int n = 50;
-    int i;
-    unsigned long int *fib = (unsigned long *)malloc(n * sizeof(unsigned long));
-    if (fib == NULL)
-    {
+	int n = 50;
+	int i;
+	unsigned long int *fib = (unsigned long *)malloc(n * sizeof(unsigned long));
+	
+	if (fib == NULL)
+	{
             perror("Memmory allocation failed");
             return (1);
-    }
-    fib[0] = 1;
-    fib[1] = 2;
-
-        for (i = 2; i < n; i++) 
-        {
+	}
+	fib[0] = 1;
+	fib[1] = 2;
+	
+	for (i = 2; i < n; i++)
+	{
         fib[i] = fib[i - 1] + fib[i - 2];
         }
+
         for (i = 0; i < n; i++)
 	{
         printf("%lu", fib[i]);
-        if (i < n - 1)
+        
+	if (i < n - 1)
         {
             printf(", ");
         
