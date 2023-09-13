6cc2f2ecfa360c8cebadfbe28220e46e0e3beec3
OrisonXmef <orisonmustapha@gmail.com>
2023-09-13 12:57:47 -0700

    102-fibonacci.c

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -21,9 +21,9 @@ int main(void)
 	for (i = 2; i < n; i++)
 	{
 		fib[i] = fib[i - 1] + fib[i - 2];
-        }
-
-        for (i = 0; i < n; i++)
+	}
+	
+	for (i = 0; i < n; i++)
 	{
 		printf("%lu", fib[i]);
 		if (i < n - 1)
