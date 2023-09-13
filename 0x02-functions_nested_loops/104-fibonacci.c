3d19a54ed6191a41af8db158f29ff76e24b5b64c
OrisonXmef <orisonmustapha@gmail.com>
2023-09-13 14:02:51 -0700

    104-fibonacci.c

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -6,14 +6,16 @@
  */
 int main(void)
 {
-	int x = 98;
+	int x = 100;
 	int i;
 	unsigned int term1 = 1;
 	unsigned int term2 = 2;
 	printf("%u, %u", term1, term2);
+
 	for (i = 0; i < x; i++)
 	{
-		unsigned int Currentterm = term1 + term2;	
+		unsigned int Currentterm = term1 + term2;
+
 		printf(", %u", Currentterm);
 		term1 = term2;
 		term2 = Currentterm;
