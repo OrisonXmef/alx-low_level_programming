d7c2d1de0bcf10251678c6f52a7f3496f9c6002f
OrisonXmef <orisonmustapha@gmail.com>
2023-09-13 18:28:27 -0700

    5-sign.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -14,13 +14,13 @@ int print_sign(int n)
 		_putchar('+');
 		return (1);
 	}
-	else if (n==0)
+	else if (n == 0)
 	{
 		_putchar('0');
 	}
-	else 
+	else
 	{
-	       	_putchar('-');
+		_putchar('-');
 		return (-1);
 	}
 	return (0);
