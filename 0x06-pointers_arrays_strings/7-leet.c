b7f1331d043b13075934460e5f840cb7e279ccb8
OrisonXmef <orisonmustapha@gmail.com>
2023-09-21 06:44:15 -0700

    7-leet.c

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * leet - encodes a string into 1337
+ * @str: the input string
+ * Return: a pointer to the resulting encoded string
+ */
+char *leet(char *str)
+{
+	int i, j;
+	char letters[] = "aAeEoOtTlL";
+	char replacements[] = "4433007711";
+		for (i = 0; str[i] != '\0'; i++)
+	{
+		for (j = 0; letters[j] != '\0'; j++)
+		{
+			if (str[i] == letters[j])
+			{
+				str[i] = replacements[j];
+				break;
+			}
+		}
+	}
+	return (str);
+}
