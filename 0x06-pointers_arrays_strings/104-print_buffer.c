17abd73747e1adecae8a71525127da98e27829fa
OrisonXmef <orisonmustapha@gmail.com>
2023-09-22 08:25:23 -0700

    104-print_buffer.c

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -1,5 +1,14 @@
 #include <stdio.h>
 
+/**
+ * print_buffer - Prints the content of a buffer in a specific format.
+ * @b: The buffer to be printed.
+ * @size: The number of bytes to print from the buffer.
+ *
+ * Description: This function prints the content of the buffer @b in a
+ * specific format. It displays 10 bytes per line, along with their
+ * hexadecimal values and ASCII representations.
+ */
 void print_buffer(char *b, int size)
 {
 	int i, j;
@@ -7,7 +16,6 @@ void print_buffer(char *b, int size)
 	for (i = 0; i < size; i += 10)
 	{
 		printf("%08x: ", i);
-
 		for (j = 0; j < 10; j++)
 		{
 			if (i + j < size)
