7536e8ce1870d00032d1e7b2b13b5571e12a6c95
OrisonXmef <orisonmustapha@gmail.com>
2023-09-22 17:57:08 -0700

    103-infinite_add.c

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -1,49 +1,60 @@
 #include <stdio.h>
 #include <string.h>
-
+/**
+ * print_buffer - Prints the content of a buffer in a specific format.
+ * @b: The buffer to be printed.
+ * @size: The number of bytes to print from the buffer.
+ exadecimal values and ASCII representations.
+ */
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
-	int len1 = strlen(n1);
-	int len2 = strlen(n2);
-	int carry = 0;
-	int i = 0;
-	int start = 0;
-	int end;
+	int x1 = 0, x2 = 0, op, bg, dr1, dr2, add = 0;
 
-	if (len1 + len2 + 1 > size_r)
+	while (*(n1 + x1) != '\0')
+			x1++;
+	while (*(n2 + x2) != '\0')
+		x2++;
+	if  (x1 >= x2)
+		bg = x1;
+	else
+		bg = x2;
+	if (size_r <= bg + 1)
+		return (0);
+	r[bg + 1] = '\0';
+	x1--, x2--, size_r--;
+
+	dr1 = *(n1 + x1) - '0';
+	dr2 = *(n2 + x2) - '0';
+
+	while (bg >= 0)
 	{
-		return (0); /*Result cannot be stored in r*/
-	}
-	while (len1 > 0 || len2 > 0 || carry > 0)
-	{
-		int digit1 = (len1 > 0) ? n1[len1 - 1] - '0' : 0;
-		int digit2 = (len2 > 0) ? n2[len2 - 1] - '0' : 0;
-		int sum = digit1 + digit2 + carry;
+		op = dr1 + dr2 + add;
+		
+		if (op >= 10)
+			add = op / 10;
 
-		carry = sum / 10;
-		r[i] = (sum % 10) + '0';
+		else
+			add = 0;
 
-		i++;
-		len1--;
-		len2--;
-	}
-	if (carry > 0)
-	{
-		r[i] = carry + '0';
-		i++;
-	}
-	r[i] = '\0';
+		if (op > 0)
+		*(r + bg) = (op % 10) + '0';
 
-	end = i - 1;
+		else
+			*(r + bg) = '0';
+		
+		if (x1 > 0)
+			x1--, dr1 = *(n1 + x1) - '0';
 
-	while (start < end)
-	{
-		char temp = r[start];
-
-		r[start] = r[end];
-		r[end] = temp;
-		start++;
-		end--;
-	}
-	return (r);
+		else
+			dr1 = 0;
+		if (x2 > 0)
+			x2--, dr2 = *(n2 + x2) - '0';
+		else
+			dr2 = 0;
+		bg--, size_r--;
+		}
+	if (*r == '0')
+		return (r + 1);
+	else
+		return (r);
 }
