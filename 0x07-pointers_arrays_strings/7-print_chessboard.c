42216dda4fc4ceeeea144dabd3b65a510d4f253b
OrisonXmef <orisonmustapha@gmail.com>
2023-09-25 09:58:19 -0700

    7-print_chessboard.c

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * print_chessboard - Prints a chessboard represented by an 8x8 character array
+ * @a: The 8x8 character array representing the chessboard.
+ *
+ * This function prints the chessboard configuration to the standard output.
+ */
+void print_chessboard(char (*a)[8])
+{
+	int i, j;
+
+	for (i = 0; i < 8; i++)
+	{
+		for (j = 0; j < 8; j++)
+		{
+			putchar(a[i][j]);
+			if (j < 7)
+				putchar(' ');
+		}
+		putchar('\n');
+	}
+}
