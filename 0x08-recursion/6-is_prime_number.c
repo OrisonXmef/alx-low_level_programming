cae8e470b59fad2f8b8e938d2a6bad419d9c7cb5
OrisonXmef <orisonmustapha@gmail.com>
2023-09-27 07:12:41 -0700

    6-is_prime_number.c

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,23 +1,37 @@
 /**
+ * is_divisible - Helper function to check if n is divisible by a divisor.
+ * @n: The number to check for divisibility.
+ * @divisor: The potential divisor to check.
+ *
+ * Return: 1 if n is divisible by divisor, 0 otherwise.
+ */
+
+int is_divisible(int n, int divisor)
+{
+	if (n == divisor)
+	{
+		return (1);
+	}
+
+	if (n % divisor == 0 || n == 1)
+	{
+		return (0);
+	}
+	return (is_divisible(n, divisor + 1));
+}
+
+/**
  * is_prime_number - Checks if an integer is a prime number.
  * @n: The integer to check.
  *
  * Return: 1 if @n is a prime number, 0 otherwise.
  */
+
 int is_prime_number(int n)
 {
-	int i;
-
 	if (n <= 1)
 	{
 		return (0);
 	}
-	for (i = 2; i * i <= n; i++)
-	{
-		if (n % i == 0)
-		{
-			return (0);
-		}
-	}
-	return (1);
+	return (is_divisible(n, 2));
 }
