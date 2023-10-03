084e5e027accb2f066eb13c7662955174966aafa
OrisonXmef <orisonmustapha@gmail.com>
2023-10-03 05:55:56 -0700

    0-create_array.c

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * create_array-Create an array of char and initialize it with a specific char.
+ * @size: The size of the array.
+ * @c: The character to initialize the array with.
+ *
+ * Return: A pointer to the created array, or NULL if it fails.
+ */
+char *create_array(unsigned int size, char c)
+{
+	char *arr;
+	unsigned int i;
+
+	if (size == 0)
+		return (NULL);
+	arr = malloc(size * sizeof(char));
+
+	if (arr == NULL)
+		return (NULL);
+
+	for (i = 0; i < size; i++)
+		arr[i] = c;
+	return (arr);
+}
---
+++ 0x0B-malloc_free/README.md
@@ -0,0 +1,12 @@
+#0x0B-malloc_free
+
+A simple C program that prints its name.
+
+## Description
+
+This program prints its name, allowing it to adapt to any name changes without recompilation.
+
+## Usage
+
+Compile the program:
+
