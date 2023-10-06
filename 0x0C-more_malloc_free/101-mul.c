f5e4d3632f98cd224ba939760abde27952aed959
OrisonXmef <orisonmustapha@gmail.com>
2023-10-05 18:08:19 -0700

    101-mul.c

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -12,7 +12,7 @@
  */
 unsigned int multiply(unsigned int num1, unsigned int num2)
 {
-        return (num1 * num2);
+	return (num1 * num2);
 }
 
 /**
@@ -23,13 +23,13 @@ unsigned int multiply(unsigned int num1, unsigned int num2)
  */
 int is_positive_number(const char *str)
 {
-        while (*str)
-        {
-                if (!isdigit(*str))
-                        return (0);
-                str++;
-        }
-        return (1);
+	while (*str)
+	{
+		if (!isdigit(*str))
+			return (0);
+		str++;
+	}
+	return (1);
 }
 
 /**
@@ -41,24 +41,24 @@ int is_positive_number(const char *str)
  */
 int main(int argc, char *argv[])
 {
-        unsigned int num1;
-        unsigned int num2;
-        unsigned int result;
+	unsigned int num1;
+	unsigned int num2;
+	unsigned int result;
 
-        if (argc != 3)
-        {
-                printf("Error\n");
-                return (98);
-    }
-        if (!is_positive_number(argv[1]) || !is_positive_number(argv[2]))
-        {
-                printf("Error\n");
-                return (98);
-        }
-        num1 = atoi(argv[1]);
-        num2 = atoi(argv[2]);
-        result = multiply(num1, num2);
-        printf("%u\n", result);
-        return (0);
+	if (argc != 3)
+	{
+		printf("Error\n");
+		return (98);
+	}
+	if (!is_positive_number(argv[1]) || !is_positive_number(argv[2]))
+	{
+		printf("Error\n");
+		return (98);
+	}
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[2]);
+	result = multiply(num1, num2);
+	printf("%u\n", result);
+	return (0);
 }
 
