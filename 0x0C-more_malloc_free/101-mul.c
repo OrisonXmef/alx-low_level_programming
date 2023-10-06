e54cc136e9b1c54140e830bea8a642b3d9a518f1
OrisonXmef <orisonmustapha@gmail.com>
2023-10-05 17:56:02 -0700

    101-mul.c

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,7 +1,7 @@
+#include <string.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <ctype.h>
-#include <gmp.h>
 
 /**
  * multiply - Multiplies two positive integers.
@@ -10,24 +10,9 @@
  *
  * Return: The result of the multiplication.
  */
-void multiply(const char *str1, const char *str2)
+unsigned int multiply(unsigned int num1, unsigned int num2)
 {
-	mpz_t num1, num2, result;
-
-	mpz_init(num1);
-	mpz_init(num2);
-	mpz_init(result);
-
-	mpz_set_str(num1, str1, 10);
-	mpz_set_str(num2, str2, 10);
-
-	mpz_mul(result, num1, num2);
-
-	gmp_printf("%Zd\n", result);
-
-	mpz_clear(num1);
-	mpz_clear(num2);
-	mpz_clear(result);
+        return (num1 * num2);
 }
 
 /**
@@ -38,13 +23,13 @@ void multiply(const char *str1, const char *str2)
  */
 int is_positive_number(const char *str)
 {
-	while (*str)
-	{
-		if (!isdigit(*str))
-			return (0);
-		str++;
-	}
-	return (1);
+        while (*str)
+        {
+                if (!isdigit(*str))
+                        return (0);
+                str++;
+        }
+        return (1);
 }
 
 /**
@@ -56,17 +41,24 @@ int is_positive_number(const char *str)
  */
 int main(int argc, char *argv[])
 {
-	if (argc != 3)
-	{
-		printf("Error\n");
-		return (98);
-	}
-	if (!is_positive_number(argv[1]) || !is_positive_number(argv[2]))
-	{
-		printf("Error\n");
-		return (98);
-	}
-	multiply(argv[1], argv[2]);
-	return (0);
+        unsigned int num1;
+        unsigned int num2;
+        unsigned int result;
+
+        if (argc != 3)
+        {
+                printf("Error\n");
+                return (98);
+    }
+        if (!is_positive_number(argv[1]) || !is_positive_number(argv[2]))
+        {
+                printf("Error\n");
+                return (98);
+        }
+        num1 = atoi(argv[1]);
+        num2 = atoi(argv[2]);
+        result = multiply(num1, num2);
+        printf("%u\n", result);
+        return (0);
 }
 
