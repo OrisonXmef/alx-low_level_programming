d843322ec479236a2dcbd5bf9b83c67e9695c191
OrisonXmef <orisonmustapha@gmail.com>
2023-10-11 17:57:02 -0700

    2-int_index.c

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -8,11 +8,12 @@
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	size_t i;
+
 	for (i = 0; i < size; i++)
 	{
 		action(array[i]);
 	}
-	
+
 }
 /**
  * print_int - Print an integer.
