195f8f8389f0dc539190fa6c70a43ed9474ec8d9
OrisonXmef <orisonmustapha@gmail.com>
2023-10-11 18:47:07 -0700

    3-calc.h

---
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -0,0 +1,32 @@
+#include "3-calc.h"
+#include <stdio.h> 
+#include <stdlib.h>
+#include <stddef.h>
+/**
+ * get_op_func - Returns a pointer to the appropriate operation function.
+ * @s: The operator as a string.
+ * Return: A pointer to the corresponding operation function.
+ */
+int (*get_op_func(char *s))(int, int)
+{
+	op_t ops[] = {
+		{"+", op_add},
+		{"-", op_sub},
+		{"*", op_mul},
+		{"/", op_div},
+		{"%", op_mod},
+		{NULL, NULL}
+	};
+	int i = 0;
+
+	while (ops[i].op)
+	{
+		if (*ops[i].op == *s && s[1] == '\0')
+		{
+			return (ops[i].f);
+		}
+		i++;
+	}
+	printf("Error\n");
+	exit(99);
+}
---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "3-calc.h"
+
+/* Function prototype */
+int (*get_op_func(char *s))(int, int);
+
+/**
+ * main - Entry point of the program.
+ * @argc: The number of command-line arguments.
+ * @argv: An array of strings containing the arguments.
+ * Return: 0 on success, 98 on error.
+ */
+
+/**
+ * main - Entry point of the program.
+ * @argc: The number of command-line arguments.
+ * @argv: An array of strings containing the arguments.
+ * Return: 0 on success, 98 on error.
+ */
+int main(int argc, char *argv[])
+{
+	int a, b;
+	int result;
+	int (*operation)(int, int);
+
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+	a = atoi(argv[1]);
+	b = atoi(argv[3]);
+			if (b == 0 && (*argv[2] == '/' || *argv[2] == '%'))
+			{
+			printf("Error\n");
+			exit(100);
+			}
+			operation = get_op_func(argv[2]);
+			result = operation(a, b);
+			printf("%d\n", result);
+			return (0);
+}
