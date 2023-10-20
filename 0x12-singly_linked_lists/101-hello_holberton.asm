eb94e8eaac8db3905ff8d543470343dc30b52169
OrisonXmef <orisonmustapha@gmail.com>
2023-10-19 18:35:13 -0700

    101-hello_holberton.asm

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,17 +1,16 @@
 section .data
-    hello db 'Hello, Holberton',0
+    format db "Hello, Holberton",10,0 ; The format string with newline
 
 section .text
     global main
     extern printf
 
 main:
-    push rdi
-    lea rdi, [hello]
-    call printf
-    pop rdi
-
-    mov rax, 60         ; Exit the program (syscall number for exit)
-    xor rdi, rdi        ; Status code 0
+    push rbp
+    mov rdi, format   ; Load the address of the format string
+    call printf       ; Call the printf function
+    add rsp, 8        ; Adjust the stack
+    mov rax, 60       ; Exit system call number (60 for 64-bit)
+    xor rdi, rdi      ; Status code 0
     syscall
 
