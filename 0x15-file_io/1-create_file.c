4ad3e78c4cfca881abe26b399574a6a84bd79c59
OrisonXmef <orisonmustapha@gmail.com>
2023-10-30 17:32:41 -0700

    1-create_file.c

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,37 @@
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdio.h>
+
+/**
+ * create_file - Creates a file with specified content.
+ * @filename: The name of the file to create.
+ * @text_content: The text content to write to the file.
+ *
+ * Return: 1 on success, -1 on failure.
+ */
+
+int create_file(const char *filename, char *text_content)
+{
+	int fd;
+	ssize_t text_len;
+
+	if (filename == NULL)
+		return (-1);
+	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
+	if (fd == -1)
+		return (-1);
+	if (text_content != NULL)
+	{
+		text_len = 0;
+		while (text_content[text_len] != '\0')
+			text_len++;
+		if (write(fd, text_content, text_len) == -1)
+		{
+			close(fd);
+			return (-1);
+		}
+	}
+	close(fd);
+	return (1);
+}
+
